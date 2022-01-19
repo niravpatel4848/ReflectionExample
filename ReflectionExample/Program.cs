74a5a13152b71376e79130220fcfdde31904b098
Neerav Patel <niravpatel4848@gmail.com>
2022-01-19 00:12:02 -0600

    Add project files.

---
+++ ReflectionExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type T = Type.GetType("ReflectionExample.Student");
+
+            Console.WriteLine("Class Name = {0}", T.Name);
+            Console.WriteLine("Namespace = {0}", T.Namespace);
+            Console.WriteLine();
+
+            Console.WriteLine("Methods in Student Class");
+
+            MethodInfo[] methods = T.GetMethods();
+
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.Name + " and it's return type is: " + method.ReturnType.Name );
+            }
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Properties in Student Class");
+
+            PropertyInfo[] properties = T.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.Name + " and it's type is: " + property.PropertyType.Name);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReflectionExample/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExample
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public void GetId()
+        {
+            Console.WriteLine("ID = {0}", this.Id);
+        }
+        public void GetName()
+        {
+            Console.WriteLine("Name = {0}", this.Name);
+        }
+    }
+}
